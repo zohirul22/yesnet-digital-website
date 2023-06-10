b72625dda291b2e267ac63e8fffb4b4092fc1b8a
zohirul22 <shahin57islam@gmail.com>
2023-06-10 11:30:39 +0600

    bannar-section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Bannar from './components/Bannar/Bannar';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div >
      <Header></Header>
+     <Bannar></Bannar>
     </div>
   );
 }
---
+++ src/components/Bannar/Bannar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Bannar.css';
+
+
+const Bannar = () => {
+    return (
+        <div className='bannar'>
+            <div className="bannar-padding">
+                <div className="bannar-title">
+                    <h2>E-LEARNING <br></br> PLATFORM</h2>
+                    <p>It is a learning and earning process by using your valuable free time at home through your smart phone only It is a very easy process and you can learn this process on your own mother tongue and you can earn from our community with selling some Courses Services or goods..</p></div>
+
+                <div className="header-btn2">
+                    <button> Login</button>
+                    <button> SignUp</button>
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+    );
+};
+
+export default Bannar;
